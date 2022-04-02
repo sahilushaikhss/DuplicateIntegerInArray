8752515b05fb770ec1f5583c8bc37aa43999581b
Sahil <sahilushaikhss@gmail.com>
2022-04-02 23:36:14 +0530

    Duplicates in String & Merger Character

--- springrest/src/main/java/com/springrest/springrest/entity/DuplicateCountsInString.java
+++ springrest/src/main/java/com/springrest/springrest/entity/DuplicateCountsInString.java
@@ -1,28 +1,38 @@
 package com.springrest.springrest.entity;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class DuplicateCountsInString {
 
     public static void main(String[] args) {
-        String s="javajavajee";
 
-        Map<Character,Integer> mp= new HashMap<>();
-        char[] chars=s.toCharArray();
+  /*   List<Integer> list= new ArrayList<>();
+     list.addAll(Arrays.asList(10,20,30,40));
+        List<Integer> list1= new ArrayList<>();
+        list.addAll(Arrays.asList(15,25,35,45));
 
-        for(Character c:chars){
-            if (mp.containsKey(c)){
-                mp.put(c, mp.get(c) + 1);
-                }
-            else{
-                mp.put(c,1);
-            }
+        List<Integer> list3= new ArrayList<>();
+        list3.addAll(list);
+        list3.addAll(list1);
+        System.out.println(list3);*/
+      /*  System.out.println(list);
+        List<Integer> integers = list.stream().map(i -> i * 10).collect(Collectors.toList());
+        System.out.println(integers);*/
+        int[]a={10,20,30};
+        int[]b={15,25,35,40,45};
+
+        int a_len=a.length;
+        int b_len=b.length;
+        int c_len=a_len+b_len;
+        int[] c= new int[c_len];
+
+        for(int i=0; i<a.length;i++){
+            c[i]=a[i];
         }
-        for(Map.Entry<Character,Integer> entry : mp.entrySet()){
-            if(entry.getValue() > 1){
-                System.out.println(entry.getKey() +"  "+ entry.getValue());
-            }
+        for(int i=0; i<b.length;i++){
+            c[a_len+i]=b[i];
         }
+        for (int i=0; i<c.length;i++){
+            System.out.print(c[i]+" ");
+        }
+
     }
 }
